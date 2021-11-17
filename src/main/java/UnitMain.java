ea25569774bba6cf6f31a299c3ad6284fe9e20cc
AndersNissum <anden21@student.sdu.dk>
2021-11-17 11:34:14 +0100

    Update UnitMain.java

--- src/main/java/UnitMain.java
+++ src/main/java/UnitMain.java
@@ -6,14 +6,19 @@ public class UnitMain {
     private UUID id;
     private String name;
 
+    //constructor if needed
+    public UnitMain(UUID id) {
+        this.id = id;
+    }
 
     public UUID getId() {
         return id;
     }
-    
+
     public void setName(String name) {
         this.name = name;
-    }
+
+        }
     public String toString() {
 
         return null;
